8e01c9b3b05a27651cbb661d6e290908fdd6dbf5
iamvishal16 <34365396+iAmVishal16@users.noreply.github.com>
2024-09-14 21:23:55 +0530

    Initial Commit

---
+++ Newsium/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Newsium
+//
+//  Created by Vishal Paliwal on 14/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Newsium/NewsiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsiumApp.swift
+//  Newsium
+//
+//  Created by Vishal Paliwal on 14/09/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
