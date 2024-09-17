4a93ad188bf09569d85e5e816753e0a5fbefdf7b
Vishal Paliwal <34365396+iAmVishal16@users.noreply.github.com>
2024-09-17 23:29:50 +0530

    Update ReadMe.swift

--- Newsium/ReadMe.swift
+++ Newsium/ReadMe.swift
@@ -11,7 +11,11 @@
 
   I'm not able to find any proper public news Api so I'm using newapi https://newsapi.org/docs/endpoints/top-headlines.
 
- - Important: This API has limit of 100/request per day.
+ - Important: 
+ - This API has limit of 100/request per day.
+ - I've used a custom card layout for the News Listing. In some places, you can see that the card has a broken UI.
+ This is because the image or content is not in the proper format by the API.
+ - I've added AppStore card Transition but due to time constraint won't able to deliver it on time
  
  - Tech Stack: SwiftUI, Combine, MVVM
 
